60b26186d39f1297acb9730a2ef6e78fa77c2d13
David Söderlund <ds@dsoderlund.consulting>
2025-09-10 20:30:59 +0200

    updated demo-fullstack-template with tilefile on ds-ref-platform too

--- ds-templates/demo-fullstack-template/content/frontend/tilt.vite.config.ts
+++ ds-templates/demo-fullstack-template/content/frontend/tilt.vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,9 +9,10 @@ export default defineConfig({
     strictPort: true,
   },
   server: {
+    allowedHosts: ['localhost', '${{ values.component }}.platform.local'],
     port: 8081,
     strictPort: true,
     host: true,
-    origin: "http://0.0.0.0:8081",
+    origin: 'http://0.0.0.0:8081',
   },
 });
